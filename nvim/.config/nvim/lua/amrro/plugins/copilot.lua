d797fc8c24852f4597970237cd87c6d93e7a1a9f
amrro <git@amrro.xyz>
2024-06-07 17:07:54 +0300

    organize plugins out of init.lua

---
+++ nvim/.config/nvim/lua/amrro/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  -- Other plugins ...
+
+  -- Add github/copilot.vim
+  {
+    'github/copilot.vim',
+    event = 'VimEnter',
+    config = function()
+      -- Optional: additional configuration for Copilot
+      -- vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+  },
+
+  -- Other plugins ...
+}
