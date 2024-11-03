24ac273715b440ceb0bbc4dd97f237e32b961c55
amrro <git@amrro.xyz>
2024-11-03 15:14:10 +0200

    Add '/target' to telescope's ignore list

---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = { "target/", "node_modules/", "%.lock" }, -- Add patterns as needed
+    },
+  },
+}
