b6fe6c5e20dd9ed6e1cf40415eb61c01201aa02d
Amr Elghobary <git@amrro.xyz>
2024-12-14 16:04:15 +0200

    Clean some zsh configs

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -2,6 +2,8 @@
 export EDITOR=nvim
 export VISUAL=nvim
 
+export GPG_TTY=$(tty)
+
 # pnpm
 export PNPM_HOME="/Users/amrro/Library/pnpm"
 case ":$PATH:" in
@@ -11,10 +13,8 @@ esac
 # pnpm end
 
 export PATH="/opt/homebrew/bin:$PATH"
-eval "$(fnm env --use-on-cd)"
 
 eval "$(starship init zsh)"
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $(brew --prefix)/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
